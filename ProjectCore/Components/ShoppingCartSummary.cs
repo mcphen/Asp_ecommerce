ced0055daa1af92ced55d225e6b35c2e87e12bbb
enock <enockmambou@gmail.com>
2021-04-13 14:43:16 +0200

    viewComponents

--- ProjectCore/Components/ShoppingCartSummary.cs
+++ ProjectCore/Components/ShoppingCartSummary.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectCore.Models;
+using ProjectCore.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProjectCore.Components
 {
-    public class ShoppingCartSummary
+    public class ShoppingCartSummary: ViewComponent 
     {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            
+            var shoppingCartViewModel = new ShoppingCartViewModel();
+            shoppingCartViewModel.ShoppingCart = _shoppingCart;
+            shoppingCartViewModel.ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal();
+            return View(shoppingCartViewModel);
+        }
     }
 }
